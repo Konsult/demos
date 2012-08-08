ebca576ad2d2d8744cd8190bf031465e4e8ddb51
Matthew Delaney <mdelaney7@gmail.com>
2012-08-07 21:16:29 -0700

    Added fleet and enemy movement and basic logic.

--- FaceInvaders/client/startup.js
+++ FaceInvaders/client/startup.js
@@ -3,9 +3,10 @@ var App = {
   time: (new Date()).getTime(),
 
   Player: null,
-  playerBullets: [],
-  fleets: [],
-  enemyBullets: [],
+  Fleet: null,
+  enemies: {},
+  playerShots: [],
+  enemyShots: [],
 
   el: null,
   w: 800, h: 600,
@@ -41,11 +42,14 @@ var App = {
     App.el.append(p.el);
   },
   loadGame: function (gameID) {
-
     switch (gameID) {
       default:
-        // Set world map
+        // Set up world map
+        App.el.css("background-color", "green");
+
         // Create enemies/fleets
+        var f = App.Fleet = new Fleet([4, 5, 6]);
+        App.el.append(f.el);
         break;
     }
   },
@@ -60,8 +64,8 @@ var App = {
     App.render();
   },
   update: function (ms) {
-    if (App.Player)
-      App.Player.update(ms);
+    App.Player && App.Player.update(ms);
+    App.Fleet && App.Fleet.update(ms);
 
     function updateAll (L) {
       for (i in L) {
@@ -69,11 +73,10 @@ var App = {
         o.update(ms);
       }
     };
-    updateAll(App.fleets);
   },
   render: function () {
-    if (App.Player)
-      App.Player.render();
+    App.Player && App.Player.render();
+    App.Fleet && App.Fleet.render();
 
     function renderAll (L) {
       for (i in L) {
@@ -81,16 +84,16 @@ var App = {
         o.render();
       }
     };
-    renderAll(App.fleets);
   }
 };
 
 function Player (id) {
   // Self State
   this.id = id;
-  this.x = this.tx = this.px = 0;
-  this.w = 100;
-  this.h = 95;
+  this.x = this.tx = 0;
+  this.w = 100; this.h = 95;
+  this.state = "alive";
+  this.deadAt = null;
 
   // Movement State
   this.moveType = "linear"; // Move via smooth linear motion
@@ -117,9 +120,6 @@ function Player (id) {
     wheelContainer.append($("<div class='wheel'>"));
   this.el.append(wheelContainer);
 
-  this.deadAt = null;
-
-  this.state = "alive";
   this.load();
 };
 Player.prototype.load = function () {
@@ -128,7 +128,6 @@ Player.prototype.load = function () {
   // Onload, set this.state = "alive";
 };
 Player.prototype.update = function (ms) {
-  this.px = this.x;
   if (this.x == this.tx)  {
     this.el.removeClass("Left Right");
     return;
@@ -198,65 +197,117 @@ Player.prototype.die = function () {
 
 
 
-
 function Fleet (ids) {
   // Self State
   this.ids = ids;
-  this.x = this.y = 0;
+  this.x = this.y = 64;
   this.w = this.h = 64 * 6;
+  this.state = "alive";
+  this.deadAt = null;
 
   // Movement State
   this.moveType = "step"; // Move by steps
   this.stepLength = 10;   // 10px wide steps
-  this.speed = 2;         // 2 steps / second
+  this.stepInterval = 500;
+  this.stepDirection = "right";
+  this.lastStep = (new Date()).getTime();
 
   // DOM State
   var el = this.el = $("<div>");
   this.el.toggleClass("Group");
   this.el.toggleClass("Fleet");
+  this.setSize(this.w, this.h);
 
   // Construct Fleet
-  this.guys = {};
+  this.numAlive = this.ids.length;
+  var guys = this.guys = {};
+  var x = 10;
+  var y = 10;
+
 	for (i in ids) {
 		var id = ids[i];
-		var guy = this.guys[id] = new Enemy(id);
-    el.append(guy.el);
+		var guy = guys[id] = App.enemies[id] = new Enemy(id);
     guy.fleet = this;
+    el.append(guy.el);
+    guy.moveTo(x, y);
+    x += 100;
 	}
 };
-Fleet.prototype.update = function(x,y) {
-  // Step the fleet when needed
+Fleet.prototype.update = function(ms) {
+  if (this.state == "dead") return;
+
+  // If all our guys die, blow ourselves up
+  if (this.numAlive == 0) {
+    this.die();
+    return;
+  }
+
+  // Compute time since last step
+  var since = App.time - this.lastStep;
+  if (since < this.stepInterval) return;
+  this.lastStep = App.time;
+
+  // Take the appropriate step
+  if (this.stepDirection == "right") {
+    if ((this.x+this.w+this.stepLength) > App.w) {
+      this.stepDown();
+      this.stepDirection = "left";
+      return;
+    }
+    this.stepRight();
+  } else {
+    if ((this.x-this.stepLength) < 0) {
+      this.stepDown();
+      this.stepDirection = "right";
+      return;
+    }
+    this.stepLeft();
+  }
+
+  // On every step, fire a random guy
+  var that = this;
+  var timeToFire = Math.random() * 500;
+  setTimeout(function () {
+
+    var num = that.numAlive * Math.random();
+    num = Math.round(num-0.5);
+    for (var i = 0; i < that.guys.length; i++) {
+      if (!num) {
+        that.guys[i].fire();
+        break;
+      }
+      if (that.guys[i].state == "alive") num--;
+    }
+
+  }, timeToFire);
 };
-Fleet.prototype.render = function(x,y) {
-  // Update the fleet's DOM
+Fleet.prototype.render = function() {
+  switch (this.state) {
+    case "alive":
+      this.el.css("left", this.x+"px");
+      this.el.css("top", this.y+"px");
+      break;
+    case "dead":
+      var since = App.time - this.deadAt;
+      if (since > 2000)
+        this.el.css("display", "none");
+      break;
+    case "loading":
+      // Show spiny or glowing character without a face?
+      break;
+  }
 };
 Fleet.prototype.stepLeft = function () {
-  if (this.tx > this.x) {
-    this.tx = this.x;
-    return;
-  }
-
-  var tx = this.tx - this.stepLength;
-  this.tx = Math.max(tx, 0);
+  this.x -= this.stepLength;
+  this.x = Math.max(this.x, 0);
 };
 Fleet.prototype.stepRight = function () {
-  if (this.tx < this.x) {
-    this.tx = this.x;
-    return;
-  }
-
-  var tx = this.tx + this.stepLength;
-  var max = App.w - this.w;
-  this.tx = Math.min(tx, max);
+  this.x += this.stepLength;
+  this.x = Math.min(this.x, App.w-this.w);
 };
 Fleet.prototype.stepDown = function () {
-  if (this.tx > this.x) {
-    this.tx = this.x;
-    return;
-  }
-
-  var tx = this.tx - this.stepLength;
-  this.tx = Math.max(tx, 0);
+  this.y += this.stepLength;
+  this.y = Math.min(this.y, App.h);
 };
 Fleet.prototype.setSize = function (w,h) {
   this.w = w;
@@ -264,11 +315,19 @@ Fleet.prototype.setSize = function (w,h) {
   this.el.width(w);
   this.el.height(h);
 };
+Fleet.prototype.die = function () {
+  this.state = "dead";
+  this.deadAt = App.time;
+  this.el.toggleClass("dead"); 
+}
 
 function Enemy (id) {
   // Self State
   this.id = id;
   this.fleet = null;
+  this.w = this.h = 64;
+  this.state = "alive";
+  this.deadAt = null;
 
   // Movement State
   this.moveType = "linear"; // Move via smooth linear motion
@@ -278,12 +337,34 @@ function Enemy (id) {
   this.el = $("<div>");
   this.el.toggleClass("Unit");
   this.el.toggleClass("Enemy");
+  this.setSize(this.w, this.h);
 };
-Enemy.prototype.moveTo = function(x,y) {
-  this.x = x;
-  this.y = y;
-  this.el.css("top", this.y+"px");
-  this.el.css("left", this.x+"px");
+Enemy.prototype.update = function(ms) {
+  // Nothing to really do by default...
+};
+Enemy.prototype.render = function() {
+  switch (this.state) {
+    case "alive":
+      break;
+    case "dead":
+      var since = App.time - this.deadAt;
+      if (since > 2000)
+        this.el.css("display", "none");
+      break;
+    case "loading":
+      // Show spiny or glowing character without a face?
+      break;
+  }
+};
+Enemy.prototype.die = function () {
+  this.state = "dead";
+  this.deadAt = App.time;
+  this.el.toggleClass("dead");
+
+  this.fleet.numAlive--;
+};
+Enemy.prototype.fire = function () {
+  console.log("Enemy fired!");
 };
 Enemy.prototype.setSize = function (w,h) {
   this.w = w;
@@ -291,9 +372,16 @@ Enemy.prototype.setSize = function (w,h) {
   this.el.width(w);
   this.el.height(h);
 };
+Enemy.prototype.moveTo = function(x,y) {
+  this.x = x;
+  this.y = y;
+  this.el.css("top", this.y+"px");
+  this.el.css("left", this.x+"px");
+};
 
 Meteor.startup(function () {
   var el = $(document.body);
   App.launch(el);
   App.loadPlayer(0);
+  App.loadGame(0);
 });
